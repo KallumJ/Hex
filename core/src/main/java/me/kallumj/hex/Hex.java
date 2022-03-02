389d99de22af32ce7f0617378d4efbbb07569898
Kallum <57623845+KallumJ@users.noreply.github.com>
2022-03-02 00:20:34 +0000

    Added texture atlas

--- core/src/main/java/me/kallumj/hex/Hex.java
+++ core/src/main/java/me/kallumj/hex/Hex.java
@@ -4,31 +4,34 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import me.kallumj.hex.textures.Atlas;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Hex extends ApplicationAdapter {
 	private SpriteBatch batch;
-	private Texture image;
+	private Atlas atlas;
 
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
-		image = new Texture("libgdx.png");
+		atlas = new Atlas();
 	}
 
 	@Override
 	public void render() {
-		Gdx.gl.glClearColor(0.15f, 0.15f, 0.2f, 1f);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(image, 140, 210);
+		Sprite sprite = atlas.getSpriteByName("edge");
+		sprite.draw(batch);
 		batch.end();
 	}
 
 	@Override
 	public void dispose() {
 		batch.dispose();
-		image.dispose();
 	}
 }
